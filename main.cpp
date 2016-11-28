247d6e21bb23c5bae919b34848e14147057fcf71
Unknown <dehodson@gmail.com>
2016-11-28 13:52:44 -0500

    Added logic for graphs E, C, and A

--- main.cpp
+++ main.cpp
@@ -10,32 +10,40 @@ void E();
 void C();
 void A();
 
+
+void throw_error(char bad_token){
+	std::cout << "\nERROR: " << bad_token << " expected.\n";
+	exit(1);
+}
+
+void throw_error(){
+	std::cout <<"\nERROR: unrecognizable symbol in input\n";
+	exit(1);
+}
+
 void getoken(){
 	std::cin.get(token);
-	std::cout << token;
+	//std::cout << token;
 	while(token ==' ' || token=='\r' || token=='\n'){
 		std::cin.get(token); 
-		std::cout << token;
+		//std::cout << token;
 	}
  	if (
 		token>='a' && token <= 'z' ||
 		token=='{'||token=='}'     ||
 		token=='('||token==')'     ||
-		token==','||token=='$' ) ; //okay
+		token==','||token=='$' );
 	else{ 
-		std::cout <<"\nERROR: unrecognizable symbol in input\n";
-		exit(1);
+		throw_error();
 	}
 }
-
-
 
 int main()
 {
 	getoken();
-	//E();
+	E();
 	if(token != '$'){
-		std::cout << "\nERROR: $ expected\n";
+		throw_error('$');
 		exit(1);
 	}
 	else std::cout <<"\nThe input is syntactically correct!\n";
@@ -43,3 +51,53 @@ int main()
 } //end main()
 
 //the functions for the parser appear below
+void E(){
+	C();
+
+	getoken();
+
+	if(token == ','){
+		C();
+	}
+	else if(token == ')' || token == '}' || token == '$');
+	else{
+		throw_error();
+	}
+}
+
+void C(){
+	A();
+
+	getoken();
+
+	if(token == ',' || token == '}' || token == ')' || token == '$');
+	else{
+		A();
+	}
+}
+
+void A(){
+	getoken();
+
+	if(token>='a' && token <= 'z');
+	else if(token == '('){
+		E();
+
+		getoken();
+
+		if(token != ')'){
+			throw_error(')');
+		}
+	}
+	else if(token == '{'){
+		E();
+
+		getoken();
+
+		if(token != '}'){
+			throw_error('}');
+		}
+	}else{
+		throw_error();
+	}
+}
